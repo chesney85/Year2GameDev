919a3bb42e68a79c5d41454aba935582063e84b0
Steven O'Neill <ghenga@gmail.com>
2017-10-03 13:24:47 +0100

    added sprites for alistair

---
+++ Project/Assets/Scripts/CoinStuff.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinStuff : MonoBehaviour {
+
+	public int points = 5;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log ("Triggered! worth " + points + " points!");
+		Destroy (gameObject);
+	}
+}
